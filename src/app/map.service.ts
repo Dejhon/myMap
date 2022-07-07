adfa774a0425607fd3b9efc70ae5aeff237cb0a1
MikesGit138 <michael138leighton@gmail.com>
2022-07-07 16:36:06 -0500

    map changes according to address

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -6,10 +6,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MapService {
-  mapkey = '065ef2421aed6d90a7bb73ee2e532679'
-  private address = '224 2 East Greater Portmore Saint Catherine'
-  //private apiUl:string = `http://api.positionstack.com/v1/forward?access_key=${this.mapkey}&query=${this.address}` 
-  apiUrl = `http://api.positionstack.com/v1/forward?access_key=${this.mapkey}&query=${this.address}`
+  private mapkey = '065ef2421aed6d90a7bb73ee2e532679'
+  //default address should be changed depending on the user input
+  private address = 'Suite B11, Pinnacle Pointe, 53 Lady Musgrave Rd, Kingston'
+  private apiUrl = `http://api.positionstack.com/v1/forward?access_key=${this.mapkey}&query=${this.address}`
 
   constructor(private httpClient: HttpClient) {
     
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,38 +10,37 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   private map: any;
   private data: any = [];
+  lat: any;
+  lon: any;
 
 
   constructor(private mapService: MapService) { }
 
 
   private initMap(): void {
-    this.mapService.sendGetRequest().subscribe((data: any[])=>{
+    this.mapService.sendGetRequest().subscribe((data: any[]) => {
       this.data = data
-      console.log(this.data.data[0].latitude)
-      // lat = this.data.data[0].latitude
-  
+      this.lat = this.data.data[0].latitude
+      this.lon = this.data.data[0].longitude
+
+      this.map = L.map('map', {
+        center: [this.lat, this.lon],
+        zoom: 10
+      });
+
+      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      });
+
+      tiles.addTo(this.map);
+      const marker = L.marker([17.97024, -76.86722])
+      marker.addTo(this.map)
     })
 
-    this.map = L.map('map', {
-      center: [ this.data.data[0].latitude, -76.86722 ],
-      zoom: 10
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
-
-    const marker = L.marker([ 17.97024, -76.86722 ])
-    marker.addTo(this.map)
 
   }
-
- 
 
   ngAfterViewInit(): void {
     this.initMap();
